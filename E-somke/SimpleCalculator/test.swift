fd181f6def5d3c5dfc8a5f333d9f564cd0431092
Piotr Żakieta <rakiet@MacBook-Air-Piotr.local>
2019-03-30 13:49:19 +0100

    test

---
+++ E-somke/SimpleCalculator/test.swift
@@ -0,0 +1,30 @@
+//
+//  test.swift
+//  E-somke
+//
+//  Created by Piotr Żakieta on 30/03/2019.
+//  Copyright © 2019 Piotr Żakieta. All rights reserved.
+//
+
+import UIKit
+
+class test: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

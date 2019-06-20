01a26a5ceeaf9958f71a17de813fab249dbee96a
Piotr Żakieta <rakiet@mac.local>
2019-06-20 16:52:33 +0200

    update v0.03, add tableView

---
+++ E-somke/Collection Liquid/DetailViewCollection.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewCollection.swift
+//  E-somke
+//
+//  Created by Piotr Żakieta on 20/06/2019.
+//  Copyright © 2019 Piotr Żakieta. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewCollection: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ E-somke/Collection Liquid/HeadCollectionCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeadCollectionCell.swift
+//  E-somke
+//
+//  Created by Piotr Żakieta on 20/06/2019.
+//  Copyright © 2019 Piotr Żakieta. All rights reserved.
+//
+
+import UIKit
+
+class HeadCollectionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
